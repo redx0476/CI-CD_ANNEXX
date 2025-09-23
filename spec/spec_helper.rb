1e6febcd026c9888d9d396e80ed4ce9e4cf8a5ed
redx0476 <harsh.g94068@gmail.com>
2025-09-23 18:26:55 +0530

    ci.yml changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,13 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require "simplecov"
+SimpleCov.start "rails" do
+  enable_coverage :branch
+  minimum_coverage ENV.fetch("SIMPLECOV_MIN_COVERAGE", 90).to_i
+  refuse_coverage_drop
+end
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
